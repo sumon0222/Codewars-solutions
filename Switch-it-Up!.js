ba1ac7687686fe4225d67a6612cee2a03bda4cd7
Mahadi Hasan (Sumon) <70533737+sumon0222@users.noreply.github.com>
2025-07-30 19:21:11 +0600

    Create Switch-it-Up!.js

---
+++ Switch-it-Up!.js
@@ -0,0 +1,4 @@
+function switchItUp(number){
+const numbers=["Zero","One", "Two","three","four","five","six","seven","eight","nine"];
+  return numbers[number].charAt(0).toUpperCase()+numbers[number].slice(1);
+}
