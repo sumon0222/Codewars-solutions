1960b9fddface5dbae9d279ebc39d0f4147a66e2
Mahadi Hasan (Sumon) <70533737+sumon0222@users.noreply.github.com>
2025-07-30 19:31:38 +0600

    another solution added to Multiples-of-3-or-5.js

--- Multiples-of-3-or-5.js
+++ Multiples-of-3-or-5.js
@@ -17,3 +17,22 @@ function solution(number){
   }
   return sum;
 }
+//another solution
+function solution(number){
+  if(number<0){
+    return 0;
+  }
+  let multiples=new Set();
+  for(let i =3; i<number;i+=3){
+  multiples.add(i) ; 
+  }
+  for(let j =5; j<number;j+=5){
+  multiples.add(j) ; 
+  }
+  
+  let sum=0;
+  multiples.forEach(num=> sum+=num);
+  return sum;
+ 
+}
+
